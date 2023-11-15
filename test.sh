9acf232a79f92d95b6c312d2beed48173ac66b64
João Carlos da Rosa <joaocarlosdarosa@MacBook-Pro-de-Joao.local>
2023-11-15 20:24:40 -0300

    fix: getting the last commited code just for files inside the src

--- test.sh
+++ test.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-PREVIOUS_COMMIT=$(git rev-parse HEAD^1)
-
 COMMITTED_CODE=""
 while read -r file; do
   # Check if the file is in the src directory
   if [[ $file == src/* ]]; then
-    COMMITTED_CODE+=$(git show $PREVIOUS_COMMIT:"$file" | tr '\n' ' ')
+    COMMITTED_CODE+=$(git show HEAD:"$file" | tr '\n' ' ')
   fi
-done < <(git diff --name-only $PREVIOUS_COMMIT HEAD)
+done < <(git diff --name-only HEAD^ HEAD)
 
 echo "Committed Code:"
 echo "$COMMITTED_CODE"
