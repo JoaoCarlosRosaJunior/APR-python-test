2a62d3a6df1e48ac0b0421df18efe1853cae5875
João Carlos da Rosa <joaocarlosdarosa@MacBook-Pro-de-Joao.local>
2023-11-15 20:35:33 -0300

    fix: another try to fix

--- test.sh
+++ test.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-PREVIOUS_COMMIT=$(git log --format="%H" -n 1 HEAD^)
-
 COMMITTED_CODE=""
-while read -r file; do
-  # Check if the file is in the src directory
-  if [[ $file == src/* ]]; then
-    COMMITTED_CODE+=$(git show $PREVIOUS_COMMIT:"$file" | tr '\n' ' ')
-  fi
-done < <(git diff --name-only $PREVIOUS_COMMIT HEAD)
-
-echo "Committed Code:"
-echo "$COMMITTED_CODE"
+  for file in $(git diff --name-only HEAD^ HEAD | grep '^src/'); do
+    COMMITTED_CODE+=$(git show HEAD:"$file" | tr '\n' ' ')
+  done
+  
+  echo "Committed Code:"
+  echo "$COMMITTED_CODE"
